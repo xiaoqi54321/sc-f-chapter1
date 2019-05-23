1b6b17e207482d6a29022858a84ec31cac7618d0
tianhao <tmh1128@foxmail.com>
2019-05-23 10:54:41 +0800

    update for config-server

---
+++ config-client/src/main/java/com/forezp/configclient/ConfigClientApplication.java
@@ -0,0 +1,27 @@
+package com.forezp.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RefreshScope //配置实时更新
+@EnableDiscoveryClient
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
